fc451ca0f01180d24af1ff01015bc7c0b8cf3aad
Mohammad Fatemi <mr.smf8@gmail.com>
2022-07-26 20:54:51 +0430

    fix: Fix redemption schema unique constraint

--- migration/20220726154833_redemptions.up.sql
+++ migration/20220726154833_redemptions.up.sql
@@ -1,6 +1,6 @@
 create table if not exists redemptions(
                                           id bigserial primary key,
-                                          voucher_code varchar(255) unique not null,
+                                          voucher_code varchar(255) not null,
                                           redeemer varchar(20) unique not null,
                                           created_at  timestamp   not null    default now()
 );
