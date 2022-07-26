d86fb1769aaa4afc1fba536358c66f896b194230
Mohammad Fatemi <mr.smf8@gmail.com>
2022-07-26 19:08:35 +0430

    feat: Setup main function to start application

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/sirupsen/logrus"
 	"github.com/smf8/arvan-voucher/internal/app/config"
 	"github.com/smf8/arvan-voucher/internal/app/handler"
+	"github.com/smf8/arvan-voucher/internal/app/model"
+	"github.com/smf8/arvan-voucher/internal/app/wallet"
 	"github.com/smf8/arvan-voucher/pkg/database"
+	"github.com/smf8/arvan-voucher/pkg/redis"
+	"github.com/smf8/arvan-voucher/pkg/router"
 	"os"
 	"os/signal"
 	"syscall"
@@ -16,27 +17,49 @@ import (
 func main() {
 	cfg := config.New()
 
-	app := fiber.New()
-
-	app.Use(cors.New())
-
-	if cfg.Debug {
-		app.Use(logger.New())
-	}
+	app := router.New(cfg.Server)
 
 	api := app.Group("/api")
-	api.Get("/healthz", handler.CheckHealth)
 
 	go func() {
-		if err := app.Listen(cfg.Port); err != nil {
+		if err := app.Listen(cfg.Server.Port); err != nil {
 			logrus.Fatalf("http server failed: %s", err.Error())
 		}
 	}()
 
-	_, err := database.NewConnection(cfg.Database)
+	db, err := database.NewConnection(cfg.Database)
 	if err != nil {
 		logrus.Fatalf("database failed: %s", err.Error())
 	}
+
+	rc, redisCloser := redis.New(cfg.Redis)
+
+	defer redisCloser()
+
+	voucherRepo := &model.SQLVoucherRepo{DB: db}
+	voucherCache := model.NewInMemoryVoucherCache(voucherRepo)
+
+	redemptionRepo := &model.SQLRedemptionRepo{DB: db}
+	voucherRemainderRepo := &model.RedisVoucherRemainderRepo{Redis: rc}
+
+	voucherHandler := handler.Voucher{
+		VoucherRepo:          voucherRepo,
+		RedemptionRepo:       redemptionRepo,
+		VoucherRemainderRepo: voucherRemainderRepo,
+	}
+
+	redeemerHandler := handler.Redeemer{
+		VoucherRemainderRepo: voucherRemainderRepo,
+		RedemptionRepo:       redemptionRepo,
+		Client:               wallet.NewClient(cfg.WalletClient),
+		VoucherCache:         voucherCache,
+	}
+
+	// Register Routes
+	api.Get("/voucher", voucherHandler.GetVoucher)
+	api.Post("/voucher", voucherHandler.Save)
+	api.Post("/voucher/redeem", redeemerHandler.RedeemVoucher)
+	api.Get("/voucher/report", voucherHandler.Report)
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
