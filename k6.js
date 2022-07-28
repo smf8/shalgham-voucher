d4490e0b2bbf29f9c980d1ce329b21b5259f6c01
Mohammad Fatemi <mr.smf8@gmail.com>
2022-07-28 19:30:51 +0430

    feat: Improve k6 test script

--- k6.js
+++ k6.js
@@ -3,55 +3,55 @@ import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 import { scenario } from 'k6/execution';
 import { check } from 'k6';
 
-let lastNumber = 0;
+// let lastNumber = 0;
 
 export const options = {
   scenarios: {
     create_profiles: {
       executor: 'ramping-arrival-rate',
       startRate: 10,
-      preAllocatedVUs: 10,
-      maxVUs: 50,
+      preAllocatedVUs: 250,
+      maxVUs: 1200,
       stages: [
-        {target: 10, duration: '20s'},
-        {target: 40, duration: '20s'},
-        {target: 100, duration: '20s'},
+        {target: 10, duration: '30s'},
+        {target: 60, duration: '30s'},
+        {target: 200, duration: '20s'},
+        {target: 1000, duration: '60s'},
       ],
       exec: 'create_profile',
       startTime: '0s'
     },
     create_voucher: {
       executor: 'shared-iterations',
-      vus: 10,
-      iterations: 50,
-      startTime: '1m',
+      vus: 50,
+      iterations: 500,
+      startTime: '0s',
       exec: 'create_voucher',
     },
     redeem_voucher: {
       executor: 'ramping-arrival-rate',
-      startRate: 20,
-      preAllocatedVUs: 10,
-      maxVUs: 50,
+      startRate: 100,
+      preAllocatedVUs: 4000,
+      maxVUs: 5000,
       stages: [
-        {target: 30, duration: '20s'},
-        {target: 50, duration: '20s'},
         {target: 100, duration: '20s'},
+        {target: 1500, duration: '10s'},
+        {target: 300, duration: '10s'},
+        {target: 3000, duration: '10s'},
+        {target: 5000, duration: '10s'},
+        {target: 100, duration: '30s'},
       ],
       exec: 'redeem_voucher',
-      startTime: '0s'
+      startTime: '30s'
     },
   },
 };
 
 export function create_profile() {
   let body = JSON.stringify({
-    phone_number: '+98913' + scenario.iterationInTest,
+    phone_number: '+98913' + (scenario.iterationInTest + 1000000),
     balance: 0,
   });
-
-  if (scenario.iterationInTest > lastNumber){
-    lastNumber = scenario.iterationInTest;
-  }
 
   const params = {
     headers: {
@@ -59,19 +59,17 @@ export function create_profile() {
     },
   };
 
-  const login_response = http.post('http://127.0.0.1:8001/api/profiles', body, params);
+  const login_response = http.post('http://40.76.146.186:8000/api/profiles', body, params);
   check(login_response, {
     'is status 200': (r) => r.status === 200,
   });
 }
 
-console.log(lastNumber)
-
 export function create_voucher() {
   let body = JSON.stringify({
     code: 'voucher_' + scenario.iterationInTest,
     amount: randomIntBetween(5000,10000),
-    limit: randomIntBetween(100,1000),
+    limit: 2000,
   });
 
   const params = {
@@ -80,15 +78,15 @@ export function create_voucher() {
     },
   };
 
-  const login_response = http.post('http://127.0.0.1:8000/api/vouchers', body, params);
+  const login_response = http.post('http://40.76.145.78:8000/api/vouchers', body, params);
   check(login_response, {
     'is status 201': (r) => r.status === 201,
   });
 }
 export function redeem_voucher() {
   let body = JSON.stringify({
-    phone_number: '+98913' + randomIntBetween(1, 2099),
-    code: 'voucher_' + randomIntBetween(1, 50)
+    phone_number: '+98913' + randomIntBetween(1000000, 1299999),
+    code: 'voucher_' + randomIntBetween(0, 499)
   });
 
   const params = {
@@ -97,7 +95,7 @@ export function redeem_voucher() {
     },
   };
 
-  const login_response = http.post('http://127.0.0.1:8000/api/vouchers/redeem', body, params);
+  const login_response = http.post('http://40.76.145.78:8000/api/vouchers/redeem', body, params);
   check(login_response, {
     'is status 200': (r) => r.status === 200,
   });
